788bc849fcbdff02220e96312a85ee15eb85f02c
voronrk <voron_76@mail.ru>
2022-10-02 12:39:00 +0300

    Put Arseniy's fix

--- js/script.js
+++ js/script.js
@@ -4,14 +4,10 @@ const anchors = document.querySelectorAll('.anchor-title');
 const menuItems = document.querySelectorAll('.menu-item');
 const btnsForms = document.querySelectorAll('.form-show');
 const btnSendOrder = document.querySelector('#send-order');
-const btnFeedback = document.querySelector('#button-feedback');
-const btnSendFeedback = document.querySelector('#send-feedback');
 
 const orderForm = document.querySelector('#order-form');
-const feedbackForm = document.querySelector('#feedback-form');
 
 const modalForm = document.querySelector('#modal-form');
-const modalFeedback = document.querySelector('#modal-feedback');
 const modalMessage = document.querySelector('#modal-message');
 const modalMessageText = document.querySelector('#modal-message-text');
 const modalClose = document.querySelectorAll('.mclose');
@@ -43,21 +39,6 @@ $(document).ready(function(){
     })
   });
 
-$(document).ready(function(){
-    $('#carousel-video').owlCarousel({
-        margin: 10,
-        loop: true,
-        items: 1,
-        nav: true,
-        dots: false,
-        video: true,
-        navText: [
-            '<img src="img/arrow_left_gray.svg" alt="left" title="left" width="40" height="43">',
-            '<img src="img/arrow_right_gray.svg" alt="left" title="left" width="40" height="43">'
-        ]
-    })
-  });
-
 btnsForms.forEach((btn) => {
     btn.addEventListener('click', () => {
         modalForm.classList.add('is-active');
@@ -67,20 +48,11 @@ btnsForms.forEach((btn) => {
 modalClose.forEach((item) => {
     item.addEventListener('click', () => {
         modalForm.classList.remove('is-active');
-        modalFeedback.classList.remove('is-active');
         modalMessage.classList.remove('is-active');
         document.querySelector('#no-name').classList.add('is-hidden');
         document.querySelector('#no-city').classList.add('is-hidden');
         document.querySelector('#no-email').classList.add('is-hidden');
         document.querySelector('#no-phone').classList.add('is-hidden');
-
-        document.querySelector('#no-name-feedback').classList.add('is-hidden');
-        document.querySelector('#no-city-feedback').classList.add('is-hidden');
-        document.querySelector('#no-email-feedback').classList.add('is-hidden');
-        document.querySelector('#no-phone-feedback').classList.add('is-hidden');
-        document.querySelector('#no-address-feedback').classList.add('is-hidden');
-        document.querySelector('#no-photo-feedback').classList.add('is-hidden');
-        document.querySelector('#no-comment-feedback').classList.add('is-hidden');
     });
 });
 
@@ -98,28 +70,6 @@ orderForm.addEventListener('submit', (e) => {
                 modalMessage.classList.add('is-active');
             } else {
                 document.querySelector('#sending-error').classList.remove('is-hidden');
-            }
-        });
-});
-
-btnFeedback.addEventListener('click', () => {
-    modalFeedback.classList.add('is-active');
-});
-
-feedbackForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    let formData = new FormData(feedbackForm);
-
-    sendFeedback(formData)
-        .then((result) => {
-            if(result) {
-                document.querySelector('#sending-error-feedback').classList.add('is-hidden');
-                modalFeedback.classList.remove('is-active');
-                feedbackForm.reset();
-                modalMessageText.innerText = 'Отзыв отправлен';
-                modalMessage.classList.add('is-active');
-            } else {
-                document.querySelector('#sending-error-feedback').classList.remove('is-hidden');
             }
         });
 });
@@ -154,56 +104,7 @@ function formValidate(formData) {
     return flag;
 }
 
-function feedbackValidate(formData) {
-    let flag = true;
-    if(!formData.name) {
-        document.querySelector('#no-name-feedback').classList.remove('is-hidden');
-        flag = false;
-    } else {
-        document.querySelector('#no-name-feedback').classList.add('is-hidden');
-    };
-    if(!formData.city) {
-        document.querySelector('#no-city-feedback').classList.remove('is-hidden');
-        flag = false;
-    } else {
-        document.querySelector('#no-city-feedback').classList.add('is-hidden');
-    };
-    if(!formData.email) {
-        document.querySelector('#no-email-feedback').classList.remove('is-hidden');
-        flag = false;
-    } else {
-        document.querySelector('#no-email-feedback').classList.add('is-hidden');
-    };
-    if(!formData.phone) {
-        document.querySelector('#no-phone-feedback').classList.remove('is-hidden');
-        flag = false;
-    } else {
-        document.querySelector('#no-phone-feedback').classList.add('is-hidden');
-    };
-    if(!formData.address) {
-        document.querySelector('#no-address-feedback').classList.remove('is-hidden');
-        flag = false;
-    } else {
-        document.querySelector('#no-address-feedback').classList.add('is-hidden');
-    };
-    if(!formData.comment) {
-        document.querySelector('#no-comment-feedback').classList.remove('is-hidden');
-        flag = false;
-    } else {
-        document.querySelector('#no-comment-feedback').classList.add('is-hidden');
-    };
-    return flag;
-}
-
 async function sendOrder(formData) {
-    const response = await fetch ('/api/mail/franchise', {
-        method: 'POST',
-        body: formData
-    });
-    return await response.json();
-};
-
-async function sendFeedback(formData) {
     const response = await fetch ('/api/mail/franchise', {
         method: 'POST',
         body: formData
